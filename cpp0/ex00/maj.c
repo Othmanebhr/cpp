d4403065b60882b3704739de761f23b5343c3c87
Othmane Bouhoura <obouhoura@gmail.com>
2025-05-07 15:43:32 +0200

    ajout V2 ex00 cpp0

---
+++ cpp0/ex00/maj.c
@@ -0,0 +1,19 @@
+#include <cctype>
+#include <iostream>
+
+int main(int ac, char **av)
+{
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; av[i]; i++)
+		{
+			std::string str = av[i];
+			for ( std::string::iterator it=str.begin(); it!=str.end(); ++it)
+			  std::cout << (char)toupper(*it);
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
