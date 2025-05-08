58d430cf7c21daacc02a148c79b348db70e65568
root <root@DESKTOP-2N7V8PB.localdomain>
2025-05-08 18:06:33 +0200

    derniers details

--- cpp1/ex04/srcs/main.cpp
+++ cpp1/ex04/srcs/main.cpp
@@ -25,13 +25,13 @@ int main(int ac, char **av)
     if (!lil_parse(ac, av))
         return 1;
 
+    //open
     std::ifstream infile(av[1]);
     if (!infile.is_open())
     {
         std::cout << "Error: cannot open file" << std::endl;
         return 1;
     }
-
     std::string outfileName = std::string(av[1]) + ".replace";
     std::ofstream outfile(outfileName.c_str());
     if (!outfile.is_open())
@@ -41,21 +41,20 @@ int main(int ac, char **av)
         return 1;
     }
 
+    //alg
     replace replace(av[2], av[3]);
     std::string content_infile((std::istreambuf_iterator<char>(infile)), 
                               std::istreambuf_iterator<char>());
     std::string find = replace.get_find();
     std::string replace_by = replace.get_replace();
-    
-    size_t pos = 0;
-    while ((pos = content_infile.find(find, pos)) != std::string::npos)
+    for (size_t pos = 0; (pos = content_infile.find(find, pos)) != std::string::npos; pos += replace_by.length())  // Change int to size_t
     {
         content_infile.erase(pos, find.length());
         content_infile.insert(pos, replace_by);
-        pos += replace_by.length();
     }
-    
     outfile << content_infile;
+    
+    //close
     infile.close();
     outfile.close();
     return 0;
