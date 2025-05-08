ee090dd3d2de4cb2ae5f3f83450ef7b3a0b28325
root <root@DESKTOP-2N7V8PB.localdomain>
2025-05-08 15:28:00 +0200

    gestion arme vide HumanB ok, ex03 ok

--- cpp1/ex03/srcs/HumanB.cpp
+++ cpp1/ex03/srcs/HumanB.cpp
@@ -1,6 +1,9 @@
 #include "../includes/HumanB.hpp"
 
-HumanB::HumanB(std::string name) : name_(name){}
+HumanB::HumanB(std::string name) : name_(name)
+{
+	weapon_ = NULL;
+}
 
 void	HumanB::setWeapon(Weapon& weapon)
 {
@@ -12,5 +15,5 @@ void	HumanB::attack()
 	if (weapon_)
 		std::cout << this->name_ << " attacks with their " << this->weapon_->get_type() << std::endl;
 	else
-	std::cout << name_ << "attck with his bare hands" << std::endl;
+		std::cout << name_ << "attck with his bare hands" << std::endl;
 }
