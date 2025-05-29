b479b82fe8740329cfd579868808807477882188
Othmane Bouhoura <obouhoura@gmail.com>
2025-05-29 21:34:33 +0200

    Main a faire + voir container

---
+++ cpp8/ex00/includes/easyfind.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+#include <exception>
+
+class NotFind : public std::exception
+{
+	public:
+	virtual const char *what() const throw();
+};
+
+const char *NotFind::what() const throw()
+{
+	return "Data not found.";
+}
+
+template<typename T>
+
+typename T::iterator easyfind(T& container, int value)
+{
+	typename T::iterator it = std::find(container.begin(), container.end(), value);
+
+	if (it == container.end())
+		throw NotFind();
+	return it;
+}
---
+++ cpp8/ex00/srcs/main.cpp
@@ -0,0 +1,6 @@
+#include "../includes/easyfind.hpp"
+
+int main()
+{
+	
+}
